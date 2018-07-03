aae24fa4f42e66b3803971b44331d351f7927379
Mark Slevinsky <funk_dev@hotmail.com>
2018-07-02 21:43:42 -0400

    Updating docs

--- public/state_machine.h
+++ public/state_machine.h
@@ -8,7 +8,7 @@
 
 --[Mark3 Realtime Platform]--------------------------------------------------
 
-Copyright (c) 2017 Funkenstein Software Consulting, all rights reserved.
+Copyright (c) 2017 - 2018 m0slevin, all rights reserved.
 See license.txt for more information
 ===========================================================================*/
 /*!
